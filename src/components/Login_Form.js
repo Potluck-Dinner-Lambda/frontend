7604bdbb82909d3ba6cdf27f0b385041640ffb94
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-07-28 12:04:44 -0700

    SShtrialcommit

--- src/components/Login_Form.js
+++ src/components/Login_Form.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, {useState} from 'react'
 import { useHistory } from 'react-router-dom';
-// import Home from './Home';
 import SignupForm from './SignupForm';
 
 
